672dafa48123e570586154594ed805853de0949d
Daniel Schruhl <danielschruhl@gmail.com>
2021-06-20 19:52:59 +0200

    Fix test

--- src/main/java/com/github/diabetesassistant/data/TokenRepository.java
+++ src/main/java/com/github/diabetesassistant/data/TokenRepository.java
@@ -1,7 +1,9 @@
 package com.github.diabetesassistant.data;
 
+import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
+@Component
 public class TokenRepository {
   public Mono<TokenEntity> create(TokenEntity entity) {
     return null;
--- src/main/java/com/github/diabetesassistant/data/UserRepository.java
+++ src/main/java/com/github/diabetesassistant/data/UserRepository.java
@@ -1,7 +1,9 @@
 package com.github.diabetesassistant.data;
 
+import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
+@Component
 public class UserRepository {
   public Mono<UserEntity> findByEmailAndPassword(String email, String password) {
     return null;
