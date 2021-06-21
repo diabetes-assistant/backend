edac4af80e74235eded9ce90d85a8c10cff2bfbe
Daniel Schruhl <danielschruhl@gmail.com>
2021-06-21 17:27:30 +0200

    Add database integration

--- src/main/java/com/github/diabetesassistant/App.java
+++ src/main/java/com/github/diabetesassistant/App.java
@@ -2,6 +2,7 @@ package com.github.diabetesassistant;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Optional;
 import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,7 +10,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class App {
   public static void main(String[] args) throws URISyntaxException {
-    URI dbUri = new URI(System.getenv("DATABASE_URL"));
+    IllegalStateException missingUrlException =
+        new IllegalStateException("DATABASE_URL environment variable not set");
+    String databaseUrl =
+        Optional.ofNullable(System.getenv("DATABASE_URL")).orElseThrow(() -> missingUrlException);
+    URI dbUri = new URI(databaseUrl);
     String username = dbUri.getUserInfo().split(":")[0];
     String password = dbUri.getUserInfo().split(":")[1];
     String dbUrl =
