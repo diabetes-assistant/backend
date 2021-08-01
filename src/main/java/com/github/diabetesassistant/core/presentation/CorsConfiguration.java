b13a07c086938ce17dc02765cd9e4f5a7f633448
Daniel Schruhl <daniel.schruhl@gropyus.com>
2021-08-01 22:54:13 +0200

    Add log for CORS

--- src/main/java/com/github/diabetesassistant/core/presentation/CorsConfiguration.java
+++ src/main/java/com/github/diabetesassistant/core/presentation/CorsConfiguration.java
@@ -1,5 +1,6 @@
 package com.github.diabetesassistant.core.presentation;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.CorsRegistry;
@@ -9,10 +10,12 @@ import org.springframework.web.reactive.config.WebFluxConfigurer;
 @ConditionalOnProperty(name = "cors", havingValue = "true")
 @Configuration
 @EnableWebFlux
+@Slf4j
 public class CorsConfiguration implements WebFluxConfigurer {
 
   @Override
   public void addCorsMappings(CorsRegistry corsRegistry) {
+    log.info("Configuring CORS");
     corsRegistry
         .addMapping("/**")
         .allowedOrigins(
