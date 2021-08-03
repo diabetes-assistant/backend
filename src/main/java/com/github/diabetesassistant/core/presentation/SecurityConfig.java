5de9afd245d0a9040950a619b22ff7ed9b873e5c
Daniel Schruhl <daniel.schruhl@gropyus.com>
2021-08-04 00:43:24 +0200

    Use spring security default cors settings

--- src/main/java/com/github/diabetesassistant/core/presentation/SecurityConfig.java
+++ src/main/java/com/github/diabetesassistant/core/presentation/SecurityConfig.java
@@ -1,15 +1,13 @@
 package com.github.diabetesassistant.core.presentation;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import com.auth0.jwt.interfaces.DecodedJWT;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.authorization.ReactiveAuthorizationManager;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
@@ -18,10 +16,9 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authorization.AuthorizationContext;
 import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
-import org.springframework.web.cors.reactive.CorsUtils;
-import org.springframework.web.server.ServerWebExchange;
-import org.springframework.web.server.WebFilter;
-import org.springframework.web.server.WebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import reactor.core.publisher.Mono;
 
 @Configuration
@@ -29,34 +26,42 @@ import reactor.core.publisher.Mono;
 @Slf4j
 public class SecurityConfig {
   private static final String[] NOT_PROTECTED_RESOURCES = new String[] {"/user/**", "/auth/**"};
-  private static final String FRONTEND_LOCALHOST = "localhost";
-  private static final String FRONTEND_STAGING = "staging-diabetes-assitant-fe.herokuapp.com";
-  private static final String FRONTEND_LIVE = "live-diabetes-assitant-fe.herokuapp.com";
-  private static final List<String> ALLOWED_ORIGINS =
-      List.of(FRONTEND_LOCALHOST, FRONTEND_STAGING, FRONTEND_LIVE);
   private static final List<String> ALLOWED_METHODS =
       List.of("PUT", "POST", "GET", "OPTION", "DELETE");
-  private static final String MAX_AGE = "3600";
+  private static final Long MAX_AGE = 3600L;
+
+  //  @Bean
+  //  public WebFilter corsFilter() {
+  //    return (ServerWebExchange ctx, WebFilterChain chain) -> {
+  //      ServerHttpRequest request = ctx.getRequest();
+  //      if (CorsUtils.isCorsRequest(request)) {
+  //        ServerHttpResponse response = ctx.getResponse();
+  //        HttpHeaders headers = response.getHeaders();
+  //        headers.add("Access-Control-Allow-Origin", "*");
+  //        headers.add("Access-Control-Allow-Methods", String.join(",", ALLOWED_METHODS));
+  //        headers.add("Access-Control-Allow-Headers", "*");
+  //        headers.add("Access-Control-Max-Age", MAX_AGE);
+  //
+  //        if (request.getMethod() == HttpMethod.OPTIONS) {
+  //          response.setStatusCode(HttpStatus.OK);
+  //          return Mono.empty();
+  //        }
+  //      }
+  //      return chain.filter(ctx);
+  //    };
+  //  }
 
   @Bean
-  public WebFilter corsFilter() {
-    return (ServerWebExchange ctx, WebFilterChain chain) -> {
-      ServerHttpRequest request = ctx.getRequest();
-      if (CorsUtils.isCorsRequest(request)) {
-        ServerHttpResponse response = ctx.getResponse();
-        HttpHeaders headers = response.getHeaders();
-        headers.add("Access-Control-Allow-Origin", "*");
-        headers.add("Access-Control-Allow-Methods", String.join(",", ALLOWED_METHODS));
-        headers.add("Access-Control-Allow-Headers", "*");
-        headers.add("Access-Control-Max-Age", MAX_AGE);
-
-        if (request.getMethod() == HttpMethod.OPTIONS) {
-          response.setStatusCode(HttpStatus.OK);
-          return Mono.empty();
-        }
-      }
-      return chain.filter(ctx);
-    };
+  CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(List.of("*"));
+    configuration.setAllowedMethods(ALLOWED_METHODS);
+    configuration.setAllowedHeaders(List.of("*"));
+    configuration.setMaxAge(MAX_AGE);
+    configuration.setAllowCredentials(true);
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
   }
 
   @Bean
@@ -85,7 +90,7 @@ public class SecurityConfig {
     http.formLogin().disable();
     http.csrf().disable();
     http.logout().disable();
-    http.cors();
+    http.cors(withDefaults());
     http.exceptionHandling();
     http.securityContextRepository(NoOpServerSecurityContextRepository.getInstance());
     http.authorizeExchange(
